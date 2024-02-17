cc2fee0169b04cb0fd9cb3d19b2ba8af155fcec6
WBeard4 <will6258@gmail.com>
2024-02-17 09:15:09 +0000

    X movement added

--- game.py
+++ game.py
@@ -13,7 +13,7 @@ class Game:
         self.display = pygame.Surface((320, 240))
         self.clock = pygame.time.Clock()
 
-        self.img_pos = (self.display.get_width() / 2), (self.display.get_height() / 2)
+        #self.img_pos = (self.display.get_width() / 2), (self.display.get_height() / 2)
         self.movement = [False, False]
         
         self.assets = {
@@ -24,12 +24,30 @@ class Game:
     def run(self):
         while True:
             self.display.fill((255, 255, 255))
+            self.player.update((self.movement[1] - self.movement[0], 0))
             self.player.render(self.display)
             
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = True
+                    if event.key == pygame.K_d:
+                        self.movement[1] = True
+                    if event.key == pygame.K_ESCAPE:
+                        pygame.quit()
+                        sys.exit()
+
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.movement[0] = False
+                    if event.key == pygame.K_d:
+                        self.movement[1] = False
+                    
+
 
             self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
             pygame.display.update()
--- scripts/entities.py
+++ scripts/entities.py
@@ -1,10 +1,21 @@
 import pygame
 
+MOVEMENT_SPEED = 1
+
 class PhysicsEntity:
     def __init__(self, game, pos, size):
         self.game = game
         self.pos = list(pos)
         self.size = size
+        self.velocity = [0, 0]
+
+
+    def update(self, movement=(0, 0)):
+        self.collisions = {'up': False, 'down': False, 'left': False, 'right': False}
+        frame_movement = (movement[0] + self.velocity[0], movement[1] + self.velocity[1])
+
+        # X movement and collision
+        self.pos[0] += frame_movement[0]
 
     def render(self, surf):
         surf.blit(self.game.assets['player'], self.pos)
